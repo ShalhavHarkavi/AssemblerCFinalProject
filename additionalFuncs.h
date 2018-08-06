6b04efa6c58b085a55e7bccff2c46576eab6b769
ShalhavHarkavi <41664058+ShalhavHarkavi@users.noreply.github.com>
2018-08-06 23:19:11 +0300

    Update additionalFuncs.h

--- additionalFuncs.h
+++ additionalFuncs.h
@@ -42,8 +42,60 @@ int *getValue(char str[], type id);
 
 char *getString(char str[], type id);
 
+typedef enum {mov, cmp, add, sub, not, clr, lea, inc, dec, jmp, bne, red, prn, jsr, rts, stop} opCode;
+
+typedef enum {r1, r2, r3, r4, r5, r6, r7, r8} registers;
+
+typedef enum {immidiate, direct, jmpWparam, directReg} addresing;
+
+typedef struct {unsigned short int ARE:2;
+                unsigned short int data:12;
+                } AREdataWord;
+
+typedef struct {unsigned short int data:14;} dataWord;
+
+typedef struct {unsigned short int ARE:2;
+                unsigned short int destination:6;
+                unsigned short int source:6;
+                } registerAdressWord;
+
+typedef struct {unsigned short int ARE:2;
+                unsigned short int destAddresing:2;
+                unsigned short int sourceAddresing:2;
+                unsigned short int op:4;
+                unsigned short int param1:2;
+                unsigned short int param2:2;
+                } instructionWord;
+
+typedef struct {unsigned short int ARE:2;
+                unsigned short int address:12;
+                } AREaddressWord;
+
+void instruction(char *str); /*public function for instruction words creation*/
+
+typedef union {AREaddressWord     AREaddress;
+               instructionWord    instruction;
+               registerAdressWord registerAddress;
+               dataWord           data;
+               AREdataWord        AREdata;
+               } word;
+
+void makeDataWords(int data); /*public function for none ARE data words creation*/
+
+typedef struct BinWordList {word Word;
+                            struct BinWordList *next;
+                            struct BinWordList *prev;
+                            } wordList;
+
+wordList *instructions; /*pointer to the first instruction*/
+wordList *data;         /*pointer to the first piece of data*/
+
 int isDataLabel(char str[]);
 
 int isInstructionLabel(char str[]);
 
 void instruction(char str[]);
+
+void error(char code);
+
+void data(label* labelData)
