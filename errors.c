966bf3c46c13dabcbf5952e4698b6571a17dbac1
LiMatan <41700469+LiMatan@users.noreply.github.com>
2018-08-19 13:59:35 +0300

    Add files via upload

--- errors.c
+++ errors.c
@@ -25,66 +25,66 @@ void error(errorCode errorType, unsigned int location, char* nameERR)
 {
   if (errorType == fopenError) {
     errorCond = Error;
-    fprintf(stderr, "AN ERROR OCCURED WHILE TRYING TO OPEN THE INPUT FILE: %s.\n",nameERR);
+    fprintf(stderr, "ERROR: AN ERROR OCCURED WHILE TRYING TO OPEN THE INPUT FILE: %s.\n",nameERR);
   }
   else if (errorType == syntaxError) {
-    fprintf(stderr, "A SYNTAX ERROR HAS BEEN DETECTED IN LINE #%d.\n", location);
+    fprintf(stderr, "ERROR: A SYNTAX ERROR HAS BEEN DETECTED IN LINE #%d.\n", location);
     errorCond = Error;
   }
   else if (errorType == nameError) {
-    fprintf(stderr, "AN ILLEGAL NAME HAS BEEN DETECTED IN LINE #%d.\n", location);
+    fprintf(stderr, "ERROR: AN ILLEGAL NAME HAS BEEN DETECTED IN LINE #%d.\n", location);
     errorCond = Error;
   }
   else if (errorType == lineLengthError) {
-    fprintf(stderr, "LINE #%d EXCEEDS THE MAXIMUM LINE LENGTH OF 80 CHARACTERS.\n", location);
+    fprintf(stderr, "ERROR: LINE #%d EXCEEDS THE MAXIMUM LINE LENGTH OF 80 CHARACTERS.\n", location);
     errorCond = Error;
   }
   else if (errorType == fileNumError) {
-    fprintf(stderr, "NO FILES HAVE BEEN ENTERED.\n");
+    fprintf(stderr, "ERROR: NO FILES HAVE BEEN ENTERED.\n");
     errorCond = Error;
   }
   else if (errorType == JmpNotLabel) {
-    fprintf(stderr, "NOT A VALID DESTINATION FOR JMP, BNE, JSR WITH PARAMETERS IN LINE #%d.\n", location);
+    fprintf(stderr, "ERROR: NOT A VALID DESTINATION FOR JMP, BNE, JSR WITH PARAMETERS IN LINE #%d.\n", location);
     errorCond = Error;
   }
   else if (errorType == param1Err) {
-    fprintf(stderr, "PARAMETER 1 IS NOT A LEGAL WORD IN LINE #%d\n.", location);
+    fprintf(stderr, "ERROR: PARAMETER 1 IS NOT A LEGAL WORD IN LINE #%d\n.", location);
     errorCond = Error;
   }
   else if (errorType == param2Err) {
-    fprintf(stderr, "PARAMETER 2 IS NOT A LEGAL WORD IN LINE #%d\n.", location);
+    fprintf(stderr, "ERROR: PARAMETER 2 IS NOT A LEGAL WORD IN LINE #%d\n.", location);
     errorCond = Error;
   }
   else if (errorType == expectComma) {
-    fprintf(stderr, "A COMMA IS EXPECTED BETWEEN PARAMETERS IN LINE #%d\n", location);
+    fprintf(stderr, "ERROR: A COMMA IS EXPECTED BETWEEN PARAMETERS IN LINE #%d\n", location);
     errorCond = Error;
   }
   else if (errorType == illegalDest) {
-    fprintf(stderr, "DESTINATION IS ILLEGAL IN LINE #%d\n", location);
+    fprintf(stderr, "ERROR: DESTINATION IS ILLEGAL IN LINE #%d\n", location);
     errorCond = Error;
   }
   else if (errorType == illegalSource) {
-    fprintf(stderr, "SOURCE IS ILLEGAL IN LINE #%d\n", location);
+    fprintf(stderr, "ERROR: SOURCE IS ILLEGAL IN LINE #%d\n", location);
     errorCond = Error;
   }
   else if (errorType == expectParen) {
-    fprintf(stderr, "EXPECTED ')' IN LINE #%d\n", location);
+    fprintf(stderr, "ERROR: EXPECTED ')' IN LINE #%d\n", location);
     errorCond = Error;
   }
   else if (errorType == expectEOL) {
-    fprintf(stderr, "THERE SHOULDN'T BE ANYTHING AFTER THE COMMAND IN LINE #%d\n", location);
+    fprintf(stderr, "WARNING: THERE SHOULDN'T BE ANYTHING AFTER THE COMMAND IN LINE #%d\n", location);
     errorCond = Warnning;
   }
   else if (errorType == EntryError) {
-    fprintf(stderr, "AN ENTRY WAS DECLARED BUT NEVER DEFINED: '%s'\n", nameERR);
+    fprintf(stderr, "ERROR: AN ENTRY WAS DECLARED BUT NEVER DEFINED: '%s'\n", nameERR);
     errorCond = Error;
   }
   else if (errorType == labelNotFound) {
-    fprintf(stderr, "A LABEL '%s' WAS USED BUT NEVER DEFINED IN LINE #%d\n", nameERR, location);
+    fprintf(stderr, "ERROR: A LABEL '%s' WAS USED BUT NEVER DEFINED IN LINE #%d\n", nameERR, location);
     errorCond = Error;
   }
   else if (errorType == externalErr) {
-    fprintf(stderr, "%s WAS DEFINED AS AN EXTERNAL BUT NEVER USED.\n", nameERR);
+    fprintf(stderr, "WARNING: %s WAS DEFINED AS AN EXTERNAL BUT NEVER USED.\n", nameERR);
     errorCond = Warnning;
   }
 }
